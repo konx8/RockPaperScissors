de98d4161479f12558271b8aec0aa280bf7a813e
konx8 <konx90@gmail.com>
2021-02-10 16:11:27 +0100

    1

---
+++ src/com/RPS/GameMechanics.java
@@ -0,0 +1,4 @@
+package com.RPS;
+
+public class GameMechanics {
+}
---
+++ src/com/RPS/Main.java
@@ -0,0 +1,8 @@
+package com.RPS;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
